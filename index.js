939f529b4984c67af400587c1fe75846fdbaebdc
uryyyyyyy <koki305@gmail.com>
2017-05-12 15:06:04 +0900

    add

---
+++ index.js
@@ -0,0 +1,48 @@
+
+const http = require('http');
+const formidable = require('formidable');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+
+  const form = new formidable.IncomingForm();
+  form.uploadDir = "./tmp";
+  console.log('hello')
+  form.parse(req, (err, fields, files) => {
+    if (err){
+      console.error(err)
+      res.write('error');
+      res.end();
+    } else {
+      const file = files['myFile']
+
+
+      const gcloud = require('google-cloud')({
+        projectId: 'uryyyyyyy-ai-executor',
+        keyFilename: './uryyyyyyy-ai-executor-78cbbb6f481e.json'
+      });
+
+      const gcs = gcloud.storage();
+      const bucket = gcs.bucket('uryyyyyyy-ai-executor');
+
+      const options = {
+        destination: `tmp/${file.name}`,
+        resumable: true,
+        validation: 'crc32c',
+        public: true
+      };
+      bucket.upload(file.path, options, (err, file) => {
+        if (err) {
+          console.error(err)
+          res.write('error');
+          res.end();
+        } else {
+          console.log(file)
+          res.write(file.toString());
+          res.end();
+        }
+      });
+    }
+  });
+}).listen(8080);
+
